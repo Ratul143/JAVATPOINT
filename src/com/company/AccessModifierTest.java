99ee5ff037233e7f56094bd81d6be42f0e02e64a
MD Mehrab <you@example.com>
2020-09-20 17:38:00 +0600

    First commit

---
+++ src/com/company/AccessModifierTest.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class AccessModifierTest {
+
+    public static void main(String[] args) {
+        Main mainObject = new Main();
+        mainObject.nonStaticTest();
+    }
+
+}
